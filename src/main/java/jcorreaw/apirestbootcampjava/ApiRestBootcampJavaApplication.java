ad3cb32c53fda8232c54134561e8b94efa2b91c5
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-21 17:59:55 -0300

    projeto criado

---
+++ src/main/java/jcorreaw/apirestbootcampjava/ApiRestBootcampJavaApplication.java
@@ -0,0 +1,13 @@
+package jcorreaw.apirestbootcampjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBootcampJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBootcampJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jcorreaw/apirestbootcampjava/ApiRestBootcampJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jcorreaw.apirestbootcampjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBootcampJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
